8da23f1da9d0ac9abf92594647fb466ca6fe3758
Denis Khamzin <96993113+DenisKhamzin@users.noreply.github.com>
2026-05-22 23:59:07 +0300

    comments added, ready to push

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -46,5 +47,101 @@ func TestCafeWhenOk(t *testing.T) {
 		handler.ServeHTTP(response, req)
 
 		assert.Equal(t, http.StatusOK, response.Code)
+	}
+}
+
+// TestCafeCount() and TestCafeSearch() have its own values for testing ecah city
+
+func TestCafeCount(t *testing.T) {
+	// defining struct for using in map
+	type results struct {
+		count int
+		want  int
+	}
+	// defining map for using in testing get-requests for both cities
+	requests := map[string][]results{
+		"moscow": {
+			{count: 0, want: 0},
+			{count: 1, want: 1},
+			{count: 2, want: 2},
+			{count: 100, want: 5}, // must show all restaurants, 5 for Moscow
+		},
+		"tula": {
+			{count: 0, want: 0},
+			{count: 1, want: 1},
+			{count: 2, want: 2},
+			{count: 100, want: 3}, // must show all restaurants, 3 for Tula
+		},
+	}
+	// testing mainHandle in main.go
+	handler := http.HandlerFunc(mainHandle)
+
+	// comparing responses for each city with map values
+	// all get-requests are without "search" parameter
+	for city, r := range requests {
+		for _, request := range r {
+			// rendering URL with unique parameters
+			url := fmt.Sprintf("/cafe?city=%s&count=%d", city, request.count)
+			response := httptest.NewRecorder()
+			req := httptest.NewRequest("GET", url, nil)
+
+			handler.ServeHTTP(response, req)
+
+			result := response.Body.String()
+			slice := strings.Split(result, `,`)
+			// exception for the "zero" result, cause length of empty slice won't be 0
+			if result == "" {
+				assert.Equal(t, request.want, 0)
+			} else {
+				assert.Equal(t, request.want, len(slice))
+			}
+		}
+	}
+}
+
+func TestCafeSearch(t *testing.T) {
+	// defining struct for using in map
+	type results struct {
+		search    string
+		wantCount int
+	}
+	// defining map for using in testing get-requests for both cities
+	requests := map[string][]results{
+		"moscow": {
+			{search: "", wantCount: 5},
+			{search: "фасоль", wantCount: 0},
+			{search: "кофе", wantCount: 2},
+			{search: "вилка", wantCount: 1},
+		},
+		"tula": {
+			{search: "", wantCount: 3},
+			{search: "мир", wantCount: 1},
+			{search: "завтрак", wantCount: 1},
+			{search: "за", wantCount: 2}, // test for the part of word
+		},
+	}
+	// testing mainhandle in main.go
+	handler := http.HandlerFunc(mainHandle)
+
+	// comparing responses for each city with map values
+	// all requests are without "count" parameter
+	for city, r := range requests {
+		for _, request := range r {
+			// rendering URL with unique parameters
+			url := fmt.Sprintf("/cafe?city=%s&search=%s", city, request.search)
+			response := httptest.NewRecorder()
+			req := httptest.NewRequest("GET", url, nil)
+
+			handler.ServeHTTP(response, req)
+
+			result := response.Body.String()
+			slice := strings.Split(result, `,`)
+			// exception for the "zero" result, cause length of empty slice won't be 0
+			if result == "" {
+				assert.Equal(t, request.wantCount, 0)
+			} else {
+				assert.Equal(t, request.wantCount, len(slice))
+			}
+		}
 	}
 }
